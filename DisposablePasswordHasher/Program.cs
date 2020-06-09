91c53c9a4d8668726c69f75912318877191845b1
Cyber_Garret <garret333@gmail.com>
2020-06-09 16:40:32 +0300

    no message

---
+++ DisposablePasswordHasher/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+using System;
+
+namespace DisposablePasswordHasher
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var passwordHasher = new PasswordHasher<string>();
+			Console.WriteLine(passwordHasher.HashPassword(null, "strong password"));
+			Console.ReadLine();
+		}
+	}
+}
